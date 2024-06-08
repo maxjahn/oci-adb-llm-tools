9dd2ee87bbf4e080bc2d92c0cc5001a720f98803
Max Jahn <max@maxjahn.at>
2024-06-08 11:42:22 +0200

    addedd compartment variable

---
+++ infrastructure/db/main.tf
@@ -0,0 +1,36 @@
+provider "oci" {}
+
+variable "compartment_ocid" {}
+
+variable "adb_admin_password" {
+  default = ""
+  sensitive = true
+}
+
+variable "adb_name" {
+  default = "ADBV"
+}
+
+variable "adb_display_name" {
+  default = "ADB-Vector"
+}
+
+resource "oci_database_autonomous_database" "oci_database_autonomous_database" {
+	admin_password = var.adb_admin_password
+	autonomous_maintenance_schedule_type = "REGULAR"
+	compartment_id = "${var.compartment_ocid}"
+	compute_count = "2"
+	compute_model = "ECPU"
+	data_storage_size_in_gb = "1024"
+	db_name = var.adb_name
+	db_version = "23ai"
+	db_workload = "OLTP"
+	display_name = var.adb_display_name
+	is_auto_scaling_enabled = "false"
+	is_auto_scaling_for_storage_enabled = "false"
+	is_dedicated = "false"
+	is_free_tier = "true"
+	is_mtls_connection_required = "true"
+	is_preview_version_with_service_terms_accepted = "false"
+	license_model = "LICENSE_INCLUDED"
+}
